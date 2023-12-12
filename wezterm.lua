d8bc723ba5cfe9642d6b1f6060a26100aca479b1
Jose Castillo Lema <josecastillolema@gmail.com>
2023-12-12 13:37:25 +0100

    Tue Dec 12 01:37:25 PM CET 2023
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -12,7 +12,6 @@ end
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
 local config = {
    --automatically_reload_config = false,
    color_scheme = 'Materia (base16)',
@@ -69,6 +68,45 @@ wezterm.on('update-status', function(window, pane)
   local overrides = window:get_config_overrides() or {}
   overrides.enable_scroll_bar = not pane:is_alt_screen_active()
   window:set_config_overrides(overrides)
+end)
+
+function recompute_padding(window)
+  local window_dims = window:get_dimensions()
+  local overrides = window:get_config_overrides() or {}
+
+  if not window_dims.is_full_screen then
+    if not overrides.window_padding then
+      -- not changing anything
+      return
+    end
+    overrides.window_padding = nil
+  else
+    -- Use only the middle 33%
+    local third = math.floor(window_dims.pixel_width / 3)
+    local new_padding = {
+      left = third,
+      right = third,
+      top = 0,
+      bottom = 0,
+    }
+    if
+      overrides.window_padding
+      and new_padding.left == overrides.window_padding.left
+    then
+      -- padding is same, avoid triggering further changes
+      return
+    end
+    overrides.window_padding = new_padding
+  end
+  window:set_config_overrides(overrides)
+end
+
+wezterm.on('window-resized', function(window, pane)
+  recompute_padding(window)
+end)
+
+wezterm.on('window-config-reloaded', function(window)
+  recompute_padding(window)
 end)
 
 -- and finally, return the configuration to wezterm
