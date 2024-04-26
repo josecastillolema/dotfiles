5cdb2060812051d635f05024b6b7cde99d17562f
Jose Castillo Lema <josecastillolema@gmail.com>
2024-04-26 12:49:17 +0200

    Fri Apr 26 12:49:17 PM CEST 2024

---
+++ upgrade.sh
@@ -0,0 +1,7 @@
+# Before running this, update the system
+
+# Verify the branch for the next major version is available
+# ostree remote refs fedora | grep sericea | grep $(uname -m)
+
+# Upgrade
+rpm-ostree rebase fedora:fedora/40/x86_64/sericea
