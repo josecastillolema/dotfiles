e9953f64b0e5befaa8673b48d32cb7812d2f908d
Jose Castillo Lema <josecastillolema@gmail.com>
2024-12-26 11:41:14 +0100

    Thu Dec 26 11:41:14 AM CET 2024
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- bashrc.d/export.sh
+++ bashrc.d/export.sh
@@ -4,7 +4,7 @@ export DOCKER_HOST=unix:///run/user/1000/docker.sock
 export EDITOR=vi
 export FZF_DEFAULT_COMMAND="fd --type f"
 #export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"  # does not work in toolbx
-export GOROOT=$HOME/go/go-1.21.3
+export GOROOT=$HOME/go/go-1.23.4
 export GOPATH=$HOME/go
 export HISTSIZE=100000                   # in memory history size
 export HISTFILESIZE=100000               # on disk history size
