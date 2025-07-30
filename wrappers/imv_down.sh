48a7711832b7443c365f4939ab48c7778232019b
Jose Castillo Lema <josecastillolema@gmail.com>
2025-07-30 11:44:28 +0200

    Wed Jul 30 11:44:28 AM CEST 2025
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- wrappers/imv_down.sh
+++ wrappers/imv_down.sh
@@ -2,9 +2,9 @@ ORIGINAL=$(swaymsg -t get_tree | jq -r '.. | select(.focused? == true).id')
 swaymsg '[app_id="thunar"] focus'
 
 # Apparently wtype does not work with Thunar :(
-wtype -k Up
+wtype -k Down
 
 # ydotool does not seem to work either (plus very complicated, daemon, sudo access, etc.)
-#ydotool key 103:1 103:0
+#ydotool key 108:1 108:0
 
 swaymsg "[con_id=$ORIGINAL] focus"
