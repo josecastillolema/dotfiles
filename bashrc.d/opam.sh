d431f66a5d7efd9570163be562d073ffe11df3b9
Jose Castillo Lema <josecastillolema@gmail.com>
2025-12-18 12:08:54 +0100

    Thu Dec 18 12:08:54 PM CET 2025
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- bashrc.d/opam.sh
+++
@@ -1,2 +0,0 @@
-test -r /var/home/jose/.opam/opam-init/init.sh && . /var/home/jose/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
-
--- bashrc.d/source.sh
+++
@@ -1,3 +0,0 @@
-#source /usr/share/fzf/shell/key-bindings.bash
-#source /etc/profile.d/vte.sh
-. "$HOME/.cargo/env"
