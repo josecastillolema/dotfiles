c9a5e472e4dda0e2792696bdf431888704c49ed8
Jose Castillo Lema <josecastillolema@gmail.com>
2025-12-18 12:49:20 +0100

    Thu Dec 18 12:49:20 PM CET 2025
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- gnupg/export.sh
+++ gnupg/export.sh
@@ -1,4 +1,4 @@
-# KEY=<KEYID> ./export.sh
+KEY=$(gpg --list-keys --with-colons | awk -F: '$1=="fpr" {print $10}' | head -n1)
 gpg --export-secret-keys $KEY > privatekey.gpg
 gpg --export $KEY > publickey.gpg
 gpg --export-ownertrust > ownertrust.txt
--- gnupg/import.sh
+++ gnupg/import.sh
@@ -1,3 +1,4 @@
 gpg --import privatekey.gpg
 gpg --import publickey.gpg
 gpg --import-ownertrust ownertrust.txt
+pass init $(cat ~/.password-store/.gpg-id)
