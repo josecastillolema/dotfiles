d78405793ad2695ad0e1e78daaca651932348adc
Jose Castillo Lema <josecastillolema@gmail.com>
2026-01-16 18:29:21 +0100

    Fri Jan 16 06:29:21 PM CET 2026
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- bash/bashrc.d/claude.sh
+++ bash/bashrc.d/claude.sh
@@ -1,5 +1,6 @@
+export ANTHROPIC_MODEL='claude-opus-4-5@20251101'
+export ANTHROPIC_VERTEX_PROJECT_ID=itpc-gcp-pnd-pe-eng-claude
 export CLAUDE_CODE_USE_VERTEX=1
 export CLOUD_ML_REGION=us-east5
-export ANTHROPIC_VERTEX_PROJECT_ID=itpc-gcp-pnd-pe-eng-claude
-export PATH=$HOME/bin/node_modules/.bin:$PATH
 export DISABLE_AUTOUPDATER=1
+export PATH=$HOME/bin/node_modules/.bin:$PATH
--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ sudo fwupdmgr update
 rpm-ostree upgrade
 flatpak uninstall --unused -y
 flatpak update -y
-toolbox run npm install @anthropic-ai/claude-code --prefix ~/bin
+toolbox run npm install @anthropic-ai/claude-code@latest --prefix ~/bin
+rm package*
 #curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir ~/bin
 #pip install --upgrade pip
 #pip list --outdated | awk 'NR>2 {print $1}' | xargs -n1 pip install -U
