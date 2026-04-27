35cd2fcc49ac26166c752f0df59c3dbeea585b05
Jose Castillo Lema <josecastillolema@gmail.com>
2026-04-27 11:23:44 +0200

    Mon 27 Apr 11:23:44 CEST 2026
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- zen/user.js
+++ zen/user.js
@@ -3,3 +3,4 @@ user_pref("browser.urlbar.suggest.bookmark", false);
 user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.maxRichResults", 0);
+user_pref("plain_text.wrap_long_lines", false);
