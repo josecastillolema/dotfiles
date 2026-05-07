dc568a6505ae2c78ae9559163186871d7350163d
Jose Castillo Lema <josecastillolema@gmail.com>
2026-05-07 16:34:22 +0200

    Thu  7 May 16:34:22 CEST 2026
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- bash/bashrc.d/function.sh
+++ bash/bashrc.d/function.sh
@@ -10,7 +10,15 @@ function grepr  { command grep --color=always -r "$@" | sed 's/:/ :/'; }
 function grepri { command grep --color=always -ri "$@" | sed 's/:/ :/'; }
 
 toolbox() {
+  local _old_term
+  if [[ "$TERM" == "xterm-ghostty" ]]; then
+    _old_term="$TERM"
+    export TERM=xterm-256color
+  fi
   /usr/sbin/toolbox "$@"
+  if [[ -n "$_old_term" ]]; then
+    export TERM="$_old_term"
+  fi
   if [[ "$1" == "enter" ]] && [ -f ~/.toolbox-last-pwd ]; then
     builtin cd "$(cat ~/.toolbox-last-pwd)"
     /usr/bin/rm -f ~/.toolbox-last-pwd
